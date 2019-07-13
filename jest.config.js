351c517401cf007bbee2e938200e00a6bbd8cb0f
Hassan Khan <hassan.khan@endemolshine.com>
2019-07-13 02:22:28 +0100

    build: update Jest and lint-staged configurations

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
-const preset = require('ts-jest').jestPreset;
-
 module.exports = {
   preset: 'ts-jest',
-  collectCoverage: false,
+  testEnvironment: 'node',
   coveragePathIgnorePatterns: [
     '/node_modules/',
     '/tests/',
@@ -13,24 +11,8 @@ module.exports = {
       tsConfig: "tsconfig.test.json",
     },
   },
-  // @TODO Uncomment the following lines once test coverage has reached an acceptable threshold
-  // 'coverageThreshold': {
-  //   'global': {
-  //     'branches': 90,
-  //     'functions': 95,
-  //     'lines': 95,
-  //     'statements': 95
-  //   }
-  // },
   moduleDirectories: [
     'src',
     'node_modules',
-  ],
-  testMatch: [
-    ...preset.testMatch,
-    '**/?(*.)+(spec|integ|test).ts?(x)',
-  ],
-  transformIgnorePatterns: [
-    '<rootDir>/node_modules/',
   ],
 };
